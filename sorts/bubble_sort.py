afad56dd973398ded83d8f78c24d7d5cb9cda6f3
bagel <freetgm@gmail.com>
2020-04-08 11:15:09 +0800

    update bubble sort
    Signed-off-by: bagel <freetgm@gmail.com>

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,17 +1,18 @@
-def swap(arr, i, j):
-    tmp = arr[i]
-    arr[i] = arr[j]
-    arr[j] = tmp
-
 def bubble_sort(arr):
     for i in range(len(arr) - 1):
+        is_changed = False
+
         for j in range(len(arr) - 1 - i):
             if arr[j] > arr[j + 1]:
-                swap(arr, j, j + 1)
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                is_changed = True
+
+        if is_changed == False:
+            break
 
     return arr
 
 
 if __name__ == "__main__":
-    arr = [1, 10, 3, 6, 2, 4, 8, 5, 12, 1, 1, 0, 13, 0]
+    arr = [1, 10, 2, 3, 8, 4, 5, 12, 0, 0, 0]
     print(bubble_sort(arr))
